e07aaa638a2a18c5f11fa5299f161fff0ddd3e56
sangala kireeti <kireetis407@gmail.com>
2025-11-23 13:54:02 +0530

    commit

--- backend/src/config/DB.config.ts
+++ backend/src/config/DB.config.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
 
 const DB=async()=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/TASK") //env
+        await mongoose.connect(process.env.URI!) //env
         console.log("DB connected....")
         
     }
